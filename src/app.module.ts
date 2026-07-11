1cab79408c169705d7409af2f3a979ed940f998b
drifterz13 <mixmyxxe@gmail.com>
2026-07-11 20:28:02 +0700

    chore: scaffold module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConcertsModule } from './concerts/concerts.module';
+import { AuditModule } from './audit/audit.module';
+import { DiscoveryModule } from './discovery/discovery.module';
+import { ReservationModule } from './reservation/reservation.module';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [],
+  imports: [ConcertsModule, AuditModule, DiscoveryModule, ReservationModule, HistoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
