deda7faf817a0ef3d56a81a5aa81a49bde27525d
drifterz13 <mixmyxxe@gmail.com>
2026-07-11 21:23:59 +0700

    chore: add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { appConfig } from './shared/config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(appConfig.port);
 }
 bootstrap();
